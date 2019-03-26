0499039ab39cd83106311ed326d2945c4805b340
Juan Diego Molina Bareño <juan.diego@kometsales.com>
2019-03-25 23:18:51 -0500

    Boostrap

--- src/app.js
+++ src/app.js
@@ -2,11 +2,18 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const hbs = require('hbs');
+const bodyParser = require('body-parser');
 require('./helpers');
 const directoriopublico = path.join(__dirname, '../public');
 const directoriopartials = path.join(__dirname, '../partials');
+const dirNode_modules = path.join(__dirname, '../node_modules')
 app.use(express.static(directoriopublico));
 hbs.registerPartials(directoriopartials);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use('/css', express.static(dirNode_modules + '/bootstrap/dist/css'));
+app.use('/js', express.static(dirNode_modules + '/jquery/dist'));
+app.use('/js', express.static(dirNode_modules + '/popper.js/dist'));
+app.use('/js', express.static(dirNode_modules + '/bootstrap/dist/js'));
 app.set('view engine', 'hbs');
 
 app.get('/', (req, res) => {
@@ -15,13 +22,20 @@ app.get('/', (req, res) => {
     });
 });
 
-app.get('/calculos', (req, res) => {
+app.post('/calculos', (req, res) => {
+    console.log(req.query);
     res.render('calculos', {
-        estudiante: 'Maria',
-        nota1: 3,
-        nota2: 4,
-        nota3: 5
+        estudiante: req.body.nombre,
+        nota1: parseInt(req.body.nota1),
+        nota2: parseInt(req.body.nota2),
+        nota3: parseInt(req.body.nota3)
     });
+});
+
+app.get('*', (req, res) => {
+    res.render('error', {
+        estudiante: 'error'
+    })
 });
 
 console.log(__dirname);
